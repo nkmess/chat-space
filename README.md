d9ca9622fd465b1b05fcce427a6e8b87848e5253
junnanakamura <junna.m_21@icloud.com>
2019-05-06 14:58:03 +0900

    Add changes to message model

--- README.md
+++ README.md
@@ -65,8 +65,8 @@ Things you may want to cover:
 |------|----|-------|
 |body|text|
 |image|string|
-|group_id|integer|null: false, foreign_key: true|
-|user_id|integer|null: false, foreign_key: true|
+|group_id|integer|foreign_key: true|
+|user_id|integer|foreign_key: true|
 
 ### Association
 - belongs_to :group
--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20190506013429) do
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",       limit: 65535
     t.string   "image"
-    t.integer  "group_id",                 null: false
-    t.integer  "user_id",                  null: false
+    t.integer  "group_id"
+    t.integer  "user_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
