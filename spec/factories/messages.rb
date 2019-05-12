6f0dab5d63c2a12ad58987b2898b32037e8bd477
junnanakamura <junna.m_21@icloud.com>
2019-05-12 10:36:49 +0900

    Test messages model

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
 
   factory :message do
     body {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    image {File.open("#{Rails.root}/public/images/dog.jpg")}
     user
     group
   end
