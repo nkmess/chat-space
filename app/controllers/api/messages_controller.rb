24356c43089dd13b7e609535fa5798957c65e285
junnanakamura <junna.m_21@icloud.com>
2019-05-26 11:40:14 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,7 @@
 class Api::MessagesController < ApplicationController
 
   def index
-    @messages = Message.where('id > ?',params[:id])
+    @messages = Message.where('id > ? and group_id = ?', params[:id], params[:group_id])
     respond_to do |format|
       format.json
     end
