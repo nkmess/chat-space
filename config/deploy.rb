4ae0c1259e704982101de3e42a911a7984a487fa
junnanakamura <junna.m_21@icloud.com>
2019-06-01 12:32:19 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :keep_releases, 5
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<2.5.1>'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/mykey/pem']
