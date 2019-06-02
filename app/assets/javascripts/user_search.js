d94645fc2f5e658d0e3fb5afba35eeacfb406969
junnanakamura <junna.m_21@icloud.com>
2019-06-02 22:04:31 +0900

    Update user_search.js

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -42,17 +42,17 @@ $(document).on('turbolinks:load', function() {
     })
   });
 
-  $("#user-search-result").on('click', ".user-search-add", function(){
-    // $("#chat-group-users").val();
+  $(document).on('click', ".user-search-add", function(){
+    $('#chat-group-users').val();
       var user_id = $(this).data('user-id');
       var user_name = $(this).data('user-name');
-    $(this).parent(".chat-group-user").remove();
-    var html = `<div class='chat-group-user clearfix js-chat-member'  id='${user_id}'>
-                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-                  <p class='chat-group-user__name'>${user_name}</p>
-                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-                </div>`
-    $("#chat-group-users").append(html);
+      var html = `<div class='chat-group-user clearfix js-chat-member'  id='${user_id}'>
+                    <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                    <p class='chat-group-user__name'>${user_name}</p>
+                    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'id='chat-group-user-${user_id}'data-user-id="${user_id}">削除</div>
+                  </div>`
+      $("#chat-group-users").append(html);
+    $(this).parent().remove();
   });
   $(document).on('click', ".user-search-remove", function(){
     $(this).parent().remove();
