887c3314a13b68b2563dfba36705431ca85cdb86
junnanakamura <junna.m_21@icloud.com>
2019-06-04 21:25:35 +0900

    Update user_search

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(function() {
 
   var search_list = $("#user-search-result");
 
@@ -19,18 +19,28 @@ $(document).on('turbolinks:load', function() {
 
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
+    var cntMembers = $('#chat-group-users').children().length;
+    var member_ids = [];
+    for (var i = 0; i < cntMembers; i++) {
+      var member_id = $('.chat-group-user input').eq(i).val();
+      member_ids.push(member_id);
+    }
+    console.log(member_ids)
     $.ajax({
       type: 'GET',
       url: '/users',
-      data: { keyword: input },
+      data: { keyword: input,
+              member_ids: member_ids
+            },
       dataType: 'json',
     })
-
+    
     .done(function(user_names) {
       $("#user-search-result").empty();
       if (user_names.length !== 0) {
         user_names.forEach(function(user_name){
           appendUserSearchHTML(user_name);
+          
         });
       }
       else {
@@ -43,10 +53,9 @@ $(document).on('turbolinks:load', function() {
   });
 
   $(document).on('click', ".user-search-add", function(){
-    $('#chat-group-users').val();
       var user_id = $(this).data('user-id');
       var user_name = $(this).data('user-name');
-      var html = `<div class='chat-group-user clearfix js-chat-member'  id='${user_id}'>
+      var html = `<div class='chat-group-user clearfix js-chat-member' id='${user_id}'>
                     <input name='group[user_ids][]' type='hidden' value='${user_id}'>
                     <p class='chat-group-user__name'>${user_name}</p>
                     <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
